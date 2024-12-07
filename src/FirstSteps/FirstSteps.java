9e4f8f670c24b786e895afdebdd744c4139b3276
Ruslan_Trifonov <ruslan.trifonov@1win.pro>
2024-12-07 19:08:09 +0500

    First Steps 2024-12-07

---
+++ src/FirstSteps/FirstSteps.java
@@ -0,0 +1,41 @@
+package FirstSteps;
+
+public class FirstSteps {
+    public static void main(String[] args) {
+        int myFirstNumber = 5;
+
+        int mySecondNumber = (10+ 5) + (2 * 500);
+
+        int myThirdNumber = 99;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int myLastOne = 1000 - myTotal;
+
+//        Try to re-declaring a variable and get an error
+//        int myFirstNumber = 100;
+
+        System.out.println("myFirstNumber value = " + myFirstNumber);
+        System.out.println("Hello World");
+        System.out.println("mySecondNumber value = " + mySecondNumber);
+        System.out.println("TOTAL VALUE: "  + myTotal);
+
+        System.out.println("LAST VALUE: "  + myLastOne);
+
+//      Primitive Types
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+
+        System.out.println("Integer min values: " + myMinIntValue);
+        System.out.println("Integer max values: " + myMaxIntValue);
+        System.out.println("Integer values range from " + myMaxIntValue + " to " + myMinIntValue);
+
+//      java: integer number too large
+//      int test = 2147483648;
+//      System.out.println(test)
+
+
+
+    }
+}
