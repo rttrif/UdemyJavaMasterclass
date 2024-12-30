a79e7e7542d4db215ba169c67fe019d30e309175
Ruslan Trifonov <ruslan.trifonov@1win.pro>
2024-12-30 22:45:27 +0500

    Section 7: OOP Part 1

--- src/OOPPart1/RecordProject/Main.java
+++ src/OOPPart1/RecordProject/Main.java
@@ -16,5 +16,33 @@ public class Main {
                     "Java Masterclass");
             System.out.println(s);
         }
+
+
+        for (int i = 1; i <= 5; i++) {
+            LPAStudent s = new LPAStudent("S92300" + i,
+                    switch (i) {
+                        case 1 -> "Mary";
+                        case 2 -> "John";
+                        case 3 -> "Jane";
+                        case 4 -> "Bob";
+                        case 5 -> "Lisa";
+                        default -> "Unknown";
+                    },
+                    "05/11/1985",
+                    "Java Masterclass");
+            System.out.println(s);
+        }
+
+        Student pojoStudent = new Student("S923006", "Ann", "05/11/1985", "Java Masterclass");
+        LPAStudent recordStudent = new LPAStudent("S923007", "Bill", "05/11/1985", "Java Masterclass");
+
+        System.out.println(pojoStudent);
+        System.out.println(recordStudent);
+
+        pojoStudent.setClassList(pojoStudent.getClassList() + ", Java OCP Exam 829");
+//        recordStudent.setClassList(recordStudent.classList() + ", Java OCP Exam 829");
+
+        System.out.println(pojoStudent.getName() + " is taking " + pojoStudent.getClassList());
+        System.out.println(recordStudent.name() + " is taking " + recordStudent.classList());
     }
 }
